6001bc89702f226c7229bcf1e2d5f89992684e0a
Arun K. Prajapati <arunkp1122@gmail.com>
2020-08-04 23:02:21 +0545

    [Fix] code fix

--- server.js
+++ server.js
@@ -15,14 +15,12 @@ const io = socketio(server);
 
 connect();
 
-f (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
    app.use(express.static("client/build"));
    app.get("*", (req, res) => {
       res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
    });
 }
-
-
 
 io.on("connection", (socket) => {
    console.log("a user connected");
@@ -39,7 +37,6 @@ io.on("connection", (socket) => {
       console.log("user left the chat");
    });
 });
-
 
 app.use("/", (req, res, next) => {
    res.status(200).json({
